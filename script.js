913a5463c5bee1b467b285a19fac4d34f66ad076
JuniorCriste <informaticode@gmail.com>
2021-10-31 16:27:25 -0300

    0.1

---
+++ script.js
@@ -0,0 +1,5 @@
+function media(num1, num2) {
+    return (num1 + num2) / 2;
+}
+var result = media(43, 90);
+console.log(result);
--- script.ts
+++ script.ts
@@ -1,1 +1,6 @@
- 
+function media(num1 : number, num2 : number) : number{
+    return (num1 + num2) / 2;
+}
+
+let result = media(43, 90);
+console.log(result);
