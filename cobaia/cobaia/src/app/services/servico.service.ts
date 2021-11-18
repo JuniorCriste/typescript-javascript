e9b296be4193e34bd18b5292f1d41ab3d86d3558
JuniorCriste <informaticode@gmail.com>
2021-11-17 21:48:27 -0300

    1.1

---
+++ cobaia/cobaia/src/app/services/servico.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicoService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getCars() : any {
+    return this.httpClient.get('https://github.com/vega/vega/blob/master/docs/data/cars.json');
+
+  }
+
+
+}
