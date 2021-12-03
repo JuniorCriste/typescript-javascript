fd66139073dd31ae73c7ffd89897dcfcc119d857
JuniorCriste <informaticode@gmail.com>
2021-12-02 21:42:10 -0300

    2.1

--- cobaiacapacitor/app/src/app/home/home.page.ts
+++ cobaiacapacitor/app/src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { ServicesService } from './../services.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,6 +8,6 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private servicesservice : ServicesService) {}
 
 }
